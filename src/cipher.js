d656d181ff94e76da08ce82f390845550b3073cc
Elisa Andrade <46823745+elisaaivarone@users.noreply.github.com>
2019-02-04 10:53:01 -0200

    Add files via upload

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,40 @@
-window.cipher = {
-  // ... 
-};
+function encodeCipher(message, offSet) {
+  let code = []
+  let codeAscii = ""
+
+  for (let i = 0; i < message.length; i++){
+    code.push(message[i]. charCodeAt());
+
+   if (codeAscii >= 65 && codeAscii <= 90){
+       codeAscii= string.fromCharCode (codeAscii - 65 + (offSet)% 26 + 65);
+   }   
+   else if (codeAscii >= 97 && codeAscii <= 122){
+       codeAscii = string.fromCharCode (codeAscii - 97 + (offSet)% 26 + 97);
+   } 
+   else {
+    return String.fromCharCode(codeAscii);
+  }
+
+ let message = document.getElementById("messageEncode"). value;
+ let offSet = parseInt (document.getElementById (offSet). value );
+
+
+ function decodeCipher(message, offSet) {
+  let code = []
+  let codeAscii = ""
+
+  for (let i = 0; i < message.length; i++){
+    code.push(message[i]. charCodeAt());
+
+   if (codeAscii >= 65 && codeAscii <= 90){
+       codeAscii= string.fromCharCode (codeAscii - 65 - (offSet)% 26 + 65);
+   }   
+   else if (codeAscii >= 97 && codeAscii <= 122){
+       codeAscii = string.fromCharCode (codeAscii - 97 - (offSet)% 26 + 97);
+   }
+   else {
+    return String.fromCharCode(codeAscii);
+  }
+ 
+ let message = document.getElementById("messageDecode"). value;
+ let offSet = parseInt (document.getElementById (offSet). value );
